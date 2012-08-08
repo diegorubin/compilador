44c57bfb0e2602c0a3e7db7b323e6834ba3b0c95
Diego Rubin <rubin.diego@gmail.com>
2012-08-08 10:45:04 -0300

    Criação do isNUM.

--- src/lexer.c
+++ src/lexer.c
@@ -3,6 +3,9 @@
 
 #include "lexer.h"
 
+#define isoctal(x) ( '0' <= (x) && (x) < '8' )
+#define ishexa(x)  ( isdigit(x) || 'a' <= tolower(x) && tolower(x) <= 'f')
+
 int isID(FILE *buffer)
 {
   char head = getc(buffer);
@@ -10,6 +13,31 @@ int isID(FILE *buffer)
     while (isalnum(head = getc(buffer)));
     ungetc(head, buffer);
     return ID;
+  }
+  ungetc(head, buffer);
+  return 0;
+}
+
+isNUM(FILE *buffer)
+{
+  char head = getc(buffer);
+  if (isdigit(head)) {
+    if (head == '0') {
+      if (tolower(head = getc(buffer)) == 'x') {
+        while (isdigit(head = getc(buffer)) || ishexa(head));
+        ungetc(head, buffer);
+        return HEXA;
+      }
+      if (isoctal(head)) {
+        while (isoctal(head = getc(buffer)));
+        ungetc(head, buffer);
+        return OCTA;
+      }
+      return ZERO;
+    }
+    while (isdigit(head = getc(buffer)));
+    ungetc(head, buffer);
+    return NUM;
   }
   ungetc(head, buffer);
   return 0;
@@ -25,6 +53,7 @@ token_t gettoken(FILE *buffer)
   ungetc(head, buffer);
 
   if(token = isID(buffer)) return token;
+  if(token = isNUM(buffer)) return token;
 
   return getc(buffer);
 }
--- tests/lexer_test.c
+++ tests/lexer_test.c
@@ -10,15 +10,37 @@
 
 int main(int argc, char **argv)
 {
-  plan_tests(1);
+  plan_tests(5);
 
   int token;
+  FILE *buffer;
+  char *input;
 
-  char input[] = "abacaxi";
-  FILE *buffer = fmemopen (input, strlen(input), "r");
+  input = "abacaxi";
+  buffer = fmemopen (input, strlen(input), "r");
   token = gettoken(buffer);
   ok1(token == ID);
-  
+
+  input = "1234";
+  buffer = fmemopen (input, strlen(input), "r");
+  token = gettoken(buffer);
+  ok1(token == NUM); 
+
+  input = "0";
+  buffer = fmemopen (input, strlen(input), "r");
+  token = gettoken(buffer);
+  ok1(token == ZERO); 
+
+  input = "0123";
+  buffer = fmemopen (input, strlen(input), "r");
+  token = gettoken(buffer);
+  ok1(token == OCTA); 
+
+  input = "0x123";
+  buffer = fmemopen (input, strlen(input), "r");
+  token = gettoken(buffer);
+  ok1(token == HEXA); 
+
   return exit_status();
 }
 
