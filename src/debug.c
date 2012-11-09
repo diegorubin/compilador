e8cedec25a7cf709278d3ee0ab290f9a53ae6216
Diego Rubin <rubin.diego@gmail.com>
2012-11-09 16:54:47 -0200

    Trabalhando na integracao com o cdb.

--- src/debug.c
+++ src/debug.c
@@ -6,7 +6,10 @@ void debug_init(void)
 
   mkfifo(gbuffer, 0666);
 
+  pthread_create(&t_cdb,NULL,call_cdb,NULL);
+
   debugfd = open(gbuffer, O_WRONLY);
+
 }
 
 void debug_finalize(void)
@@ -23,5 +26,22 @@ void debug_send_message(char *message)
 int debug_isinitialized(void)
 {
   return debugfd;
+}
+
+void *call_cdb()
+{
+  char *arguments[1];
+  int pid; 
+
+  printf("opa\n");
+
+  pid = fork();
+  if(!pid){
+    arguments[0] = strdup("/usr/bin/cdb");
+    execv("/usr/bin/cdb",arguments);
+  }else{
+    fprintf(stderr, "Não foi possível executar o debugger.\n");
+    exit(1);
+  }
 }
 
--- src/debug.h
+++ src/debug.h
@@ -1,10 +1,13 @@
 #ifndef __MYPAS_DEBUG_H__
 #define __MYPAS_DEBUG_H__
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <string.h>
 
 enum {
@@ -21,6 +24,12 @@ void debug_finalize();
 void debug_send_message(char *message);
 
 int debug_isinitialized();
+
+/* threads */
+pthread_t t_cdb;
+
+/* Methods */
+void *call_cdb();
 
 #endif //__MYPAS_DEBUG_H__
 
